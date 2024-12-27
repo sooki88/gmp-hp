e17434c0feb2c7de70b2850ede630d441cf83152
굿모닝페이 <gmp@gusmoningpeiui-iMac.local>
2024-12-27 10:23:23 +0900

    라이브서버 실패

--- static/script/header.js
+++ static/script/header.js
@@ -1,6 +1,6 @@
 // 헤더 자바스크립트
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("./components/header.html")
+  fetch("components/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.querySelector("#header-area").innerHTML = data;
@@ -26,12 +26,12 @@ document.addEventListener("DOMContentLoaded", () => {
             link.classList.add("light");
             link.classList.remove("dark");
           });
-          logo.src = "../images/logo_light.svg";
+          logo.src = "images/logo_light.svg";
           loginButtons.forEach((button) => {
             button.classList.add("light");
             button.classList.remove("dark");
           });
-          menuButton.src = "../images/menu_light.svg";
+          menuButton.src = "images/menu_light.svg";
 
           // 현재 페이지가 darkPath일 경우
         } else if (darkPaths.some((darkPath) => path === darkPath)) {
@@ -40,12 +40,12 @@ document.addEventListener("DOMContentLoaded", () => {
             link.classList.add("dark");
             link.classList.remove("light");
           });
-          logo.src = "../images/logo_dark.svg";
+          logo.src = "images/logo_dark.svg";
           loginButtons.forEach((button) => {
             button.classList.add("dark");
             button.classList.remove("light");
           });
-          menuButton.src = "../images/menu_dark.svg";
+          menuButton.src = "images/menu_dark.svg";
         }
       };
 
